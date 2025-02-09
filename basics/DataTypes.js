6de3f137f792bcadcdb85e1828e10d14cd2c1ba6
Arpan Jain <arpanjain595@gmail.com>
2025-02-09 05:46:01 +0000

    DataTypes

---
+++ basics/DataTypes.js
@@ -0,0 +1,28 @@
+"use strict";
+ //treat all JS code as newer version
+// alert("hello") we are using nodejs not browser
+console.log(3+3)
+ console.log("Arpan")
+
+ let nam="Arpan" 
+ let age=18
+ let isloggedin=false
+ let state;
+//Primitive Data Type
+
+ // number => 2 to power 53
+ // bigint 
+ //string => ""
+ // boolean => true/false
+ // null => stand alone value
+ // undefined => declared but not defined
+ // symbol => uniqueness finding
+
+ //object
+
+ console.log(typeof age);
+ console.log(typeof nam);
+ console.log(typeof isloggedin);
+ console.log(typeof state);
+ console.log(typeof undefined);
+ console.log(typeof null);
